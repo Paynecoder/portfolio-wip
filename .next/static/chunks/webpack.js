9d74d247479a17afe6bf5907810328178f54cff0
Joshua Payne <payne@Joshuas-MacBook-Pro.local>
2023-02-24 12:04:09 -0800

    stack

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "05505caedc8df5ca"; }
+/******/ 		__webpack_require__.h = function() { return "5bf89dbc8153dd2a"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- pages/stack.js
+++ pages/stack.js
@@ -4,7 +4,33 @@ export default function Stack() {
   return (
     <>
       <h1 className={styles.code}>&lt;div id="stack"&gt;</h1>
-      <div className={styles.main}></div>
+      <div className={styles.main}>
+        <h1 className={styles.title}>My Stack</h1>
+        <div className={styles.wrapper}>
+          <div>
+            <h2 className={styles.subtitle}>Front-end</h2>
+            <ul className={styles.list}>
+              <li className={styles.items}>HTML</li>
+              <li className={styles.items}>CSS</li>
+              <li className={styles.items}>JavaScript</li>
+              <li className={styles.items}>React</li>
+              <li className={styles.items}>Next.js</li>
+            </ul>
+          </div>
+          <div>
+            <h2 className={styles.subtitle}>Back-end</h2>
+            <ul className={styles.list}>
+              <li className={styles.items}>Node.js</li>
+            </ul>
+          </div>
+          <div>
+            <h2 className={styles.subtitle}>Others</h2>
+            <ul className={styles.list}>
+              <li className={styles.items}>Git</li>
+            </ul>
+          </div>
+        </div>
+      </div>
       <h1 className={styles.code}>&lt;/div&gt;</h1>
     </>
   );
